7ee5a8b38c4a585b29969a5e435083f05414320c
do0ik <oulwind00@naver.com>
2026-02-03 23:09:55 +0900

    fix: 전역 레이아웃 SEO 정보 수정, 회사소개 페이지 브랜드명 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: {
-    default: 'Lodgense - 숙박업 위탁운영 & 컨설팅',
-    template: '%s | Lodgense',
+    default: 'SoUHGM - 숙박업 위탁운영 & 컨설팅',
+    template: '%s | SoUHGM',
   },
   description:
     '숙박업 위탁운영 및 전문 컨설팅 서비스를 통해 숙박업의 가치를 극대화합니다.',
@@ -22,20 +22,20 @@ export const metadata: Metadata = {
     '숙박업 경영',
     '숙박업 컨설팅',
   ],
-  authors: [{ name: 'Lodgense' }],
-  creator: 'Lodgense',
+  authors: [{ name: 'SoUHGM' }],
+  creator: 'SoUHGM',
   openGraph: {
     type: 'website',
     locale: 'ko_KR',
-    url: 'https://lodgense.com',
-    title: 'Lodgense - 숙박업 위탁운영 & 컨설팅',
+    url: 'https://souhgm.com',
+    title: 'SoUHGM - 숙박업 위탁운영 & 컨설팅',
     description:
       '숙박업 위탁운영 및 전문 컨설팅 서비스를 통해 숙박업의 가치를 극대화합니다.',
-    siteName: 'Lodgense',
+    siteName: 'SoUHGM',
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'Lodgense - 숙박업 위탁운영 & 컨설팅',
+    title: 'SoUHGM - 숙박업 위탁운영 & 컨설팅',
     description:
       '숙박업 위탁운영 및 전문 컨설팅 서비스를 통해 숙박업의 가치를 극대화합니다.',
   },
@@ -73,7 +73,7 @@ export default function RootLayout({
         {/* Apple specific meta tags */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Lodgense" />
+        <meta name="apple-mobile-web-app-title" content="SoUHGM" />
         {/* Microsoft specific meta tags */}
         <meta name="msapplication-TileColor" content="#1C2A44" />
         <meta name="msapplication-config" content="none" />
